bbc3af7c3317ab70c2df8a0e949925482e2d516e
bo zhang <maxutility2011@gmail.com>
2024-08-06 15:46:48 -0400

    use 4 digits to represent frame number

--- video_upscaler.py
+++ video_upscaler.py
@@ -13,7 +13,8 @@ pipeline = pipeline.to("cuda")
 # let's download the images from the base url given by the command line argument
 url_base = sys.argv[1]
 for i in range (1, 75):
-    url = url_base + str(i) + ".png"
+    url = url_base + "/image_" + ("%04d" % i) + ".png"
+    print(url)
 
     #url = "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd2-upscale/low_res_cat.png"
     response = requests.get(url)
@@ -23,5 +24,5 @@ for i in range (1, 75):
     prompt = ""
 
     upscaled_image = pipeline(prompt=prompt, image=low_res_img, num_inference_steps=75).images[0]
-    output = "./new1/highres/new1_" + str(i) + "_highres.png"
+    output = "./highres/image_" + ("%04d" % i) + ".png"
     upscaled_image.save(output)
